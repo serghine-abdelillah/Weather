e2cbc91a5f5abcc199124fce2e666ee2788eeba9
Abdelillah Serghine <abdelillah2003@gmail.com>
2025-08-12 11:47:03 +0100

    build, core: update HTML and JS Files

--- main.js
+++ main.js
@@ -1,21 +1,51 @@
-const cityName = 'Adrar';
-const status = 'Cloudy';
+const statusVal = 'Foggy';
 const degree = 23;
 const wind = 40 ;
 const humidity = 40;
 const searchBtn = document.querySelector('.search-box img');
+const inputEle = document.querySelector('.search-box input');
 const cityEle = document.querySelector('.city p');
 const statusImg = document.querySelector('.status img');
-const degreeEle = document.querySelector('.status p');
+const statusTxt = document.querySelector('.status-text');
+const degreeEle = document.querySelector('.degree');
 const humidityEle = document.querySelector('.humidity p');
 const windEle = document.querySelector('.wind p');
 
 searchBtn.addEventListener('click', function () {
+    const cityName = inputEle.value;
     cityEle.innerText = cityName;
+    statusTxt.innerText = statusVal;
     degreeEle.innerText = degree+'°';
     windEle.innerText = wind+' km/h';
     humidityEle.innerText = humidity+'%';
-    // statusImg.className.valueOf.;
+    switch (statusVal) {
+        case 'Cloudy':
+            statusImg.setAttribute('src', 'Assets/cloudy.gif');
+            break;
+            case 'Sunny':
+            statusImg.setAttribute('src', 'Assets/sunny.gif');
+            
+            break;
+            case 'Foggy':
+            statusImg.setAttribute('src', 'Assets/foggy.gif');
+            
+            break;
+            case 'Rainy':
+            statusImg.setAttribute('src', 'Assets/rainy.gif');
+            
+            break;
+            case 'Snow':
+            statusImg.setAttribute('src', 'Assets/snow.gif');
+            
+            break;
+            case 'Stormy':
+            statusImg.setAttribute('src', 'Assets/stormy.gif');
+            
+            break;
+    
+        default:
+            break;
+    }
 
 
 })
