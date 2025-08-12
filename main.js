fa5b58ef1867546628230a887780cb4a70db0ae8
Abdelillah Serghine <abdelillah2003@gmail.com>
2025-08-12 11:28:09 +0100

    build, core: update HTML and JS Files

--- main.js
+++ main.js
@@ -3,3 +3,19 @@ const status = 'Cloudy';
 const degree = 23;
 const wind = 40 ;
 const humidity = 40;
+const searchBtn = document.querySelector('.search-box img');
+const cityEle = document.querySelector('.city p');
+const statusImg = document.querySelector('.status img');
+const degreeEle = document.querySelector('.status p');
+const humidityEle = document.querySelector('.humidity p');
+const windEle = document.querySelector('.wind p');
+
+searchBtn.addEventListener('click', function () {
+    cityEle.innerText = cityName;
+    degreeEle.innerText = degree+'°';
+    windEle.innerText = wind+' km/h';
+    humidityEle.innerText = humidity+'%';
+    // statusImg.className.valueOf.;
+
+
+})
