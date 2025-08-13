c6ff7e45bd8b098d736db71f7f2b9241e36aeb67
Abdelillah Serghine <abdelillah2003@gmail.com>
2025-08-13 10:05:53 +0100

    core: Update JS File

--- main.js
+++ main.js
@@ -1,41 +1,21 @@
 
 const APIKEY = 'e7d5382aa3d6728a13e12f0585df06e1';
-const degree = 23;
-const wind = 40 ;
-const humidity = 40;
+const degree = null;
+const wind = null;
+const humidity = null;
 let statusVal = '';
 const searchBtn = document.querySelector('.search-box img');
 const inputEle = document.querySelector('.search-box input');
 const card = document.querySelector('.card-box');
+const cityEle = document.querySelector('.city p');
+const statusImg = document.querySelector('.status img');
+const statusTxt = document.querySelector('.status-text');
+const degreeEle = document.querySelector('.degree');
+const humidityEle = document.querySelector('.humidity p');
+const windEle = document.querySelector('.wind p');
 searchBtn.addEventListener('click', function () {
     const cityName = inputEle.value;
-    // card.innerHTML += `<div class="city"><img class="icon" src="Assets/pin.png" alt="location-icon"><p></p></div><div class="status">
-    //         <!-- sunny cloudy  -->
-    //         <img src="" alt="status"> 
-    //         <p class="status-text"></p>
-    //         <!-- degree -->
-    //         <p class="degree"></p>
-    //     </div>
-    //     <div class="others">
-    //         <div class="humidity">
-    //             <!-- humidity -->
-    //             <img class="icon" src="Assets/humidity.png" alt="humidity-icon">
-    //             <p></p>
-    //         </div>
-    //         <div class="wind">
-    //             <!-- wind -->
-    //             <img class="icon" src="Assets/wind.png" alt="wind-icon">
-    //             <p></p>
-    //         </div>
-    //     </div>`
     getWeather(cityName).then(apiData => {
-        
-        const cityEle = document.querySelector('.city p');
-        const statusImg = document.querySelector('.status img');
-        const statusTxt = document.querySelector('.status-text');
-        const degreeEle = document.querySelector('.degree');
-        const humidityEle = document.querySelector('.humidity p');
-        const windEle = document.querySelector('.wind p');
         cityEle.innerText = apiData.name;
         degreeEle.innerText = Math.round(apiData.main.temp - 273.15 )+'°C';
         windEle.innerText = Math.round(apiData.wind.speed)+' km/h';
